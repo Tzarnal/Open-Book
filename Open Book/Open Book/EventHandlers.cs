a4a025d6d002af9c14efaf3d0e12c8bfd19af397
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-01-29 17:56:41 +0100

    Improved layout, options menu now functional.

---
+++ Open Book/Open Book/EventHandlers.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+[EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+public static class EventHandlers
+{
+}
