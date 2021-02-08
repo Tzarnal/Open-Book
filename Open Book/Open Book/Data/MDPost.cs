67eade39b143a2a9989e24dc9969d102a2f6cc80
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-02-08 18:07:46 +0100

    Poco for holding md post data

---
+++ Open Book/Open Book/Data/MDPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Open_Book.Data
+{
+    public class MDPost
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        public string File { get; set; }
+
+        public string Id { get; set; }
+        public string Url { get; set; }
+    }
+}
