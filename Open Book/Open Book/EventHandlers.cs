8ade5ca9f3a893e38e9cb1b2ff1745cc61e4273e
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2021-02-09 11:57:14 +0100

    Hooking up Blazor Fluid UI

--- Open Book/Open Book/EventHandlers.cs
+++ Open Book/Open Book/EventHandlers.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
-[EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
-[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
 public static class EventHandlers
 {
 }
